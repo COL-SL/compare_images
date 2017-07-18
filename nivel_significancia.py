5f9e538d84b88760896b91318425d4c77814ab90
COL-SL <manueldavid83@gmail.com>
2017-07-18 22:59:32 +0200

    demostrado paro/presos

--- nivel_significancia.py
+++ nivel_significancia.py
@@ -1,13 +1,15 @@
 import numpy as np
 import sys
 import math as mt
+from scipy.stats import levene, shapiro
 
 
 def covarianza():
-    #paro = [22.1, 20.6, 18.6, 15.6, 13.9, 10.63, 11.61, 11.37, 10.53, 8.71, 8.26, 8.57, 13.79, 18.66, 20.11, 22.56, 25.77, 25.73, 23.70, 20.90]
-    #pres = [41.90, 42.75, 44.37, 44.19, 45.10, 47.57, 51.88, 56.09, 59.37, 61.05, 64.02, 67.1, 73.55, 76.07, 73.92, 70.47, 68.59, 66.76, 65.01, 61.61]
-    paro = [13.9, 10.63, 11.61, 11.37, 10.53, 8.71, 8.26, 8.57, 13.79, 18.66, 20.11, 22.56, 25.77, 25.73, 23.70, 20.90]
-    pres = [45.10, 47.57, 51.88, 56.09, 59.37, 61.05, 64.02, 67.1, 73.55, 76.07, 73.92, 70.47, 68.59, 66.76, 65.01, 61.61]
+    paro = [22.1, 20.6, 18.6, 15.6, 13.9, 10.63, 11.61, 11.37, 10.53, 8.71, 8.26, 8.57, 13.79, 18.66, 20.11, 22.56, 25.77, 25.73, 23.70, 20.90]
+    pres_paro_menor_16 = [44.19, 45.10, 47.57, 51.88, 56.09, 59.37, 61.05, 64.02, 67.1]
+    pres_paro_mayor_16 = [41.90, 42.75, 44.37, 76.07, 73.92, 70.47, 68.59, 66.76, 65.01,61.61]
+    #paro = [13.9, 10.63, 11.61, 11.37, 10.53, 8.71, 8.26, 8.57, 13.79, 18.66, 20.11, 22.56, 25.77, 25.73, 23.70, 20.90]
+    #pres = [44.19, 45.10, 47.57, 51.88, 56.09, 59.37, 61.05, 64.02, 67.1, 73.55, 76.07, 73.92, 70.47, 68.59, 66.76, 65.01, 61.61]
     #paro = [10.63, 11.61, 11.37, 10.53, 8.71, 8.26, 8.57, 13.79, 18.66, 20.11, 22.56, 25.77, 25.73, 23.70, 20.90]
     #pres = [47.57, 51.88, 56.09, 59.37, 61.05, 64.02, 67.1, 73.55, 76.07, 73.92, 70.47, 68.59, 66.76, 65.01, 61.61]
 
@@ -21,18 +23,34 @@ def covarianza():
 
     #paro = [6, 4, 8, 9, 4, 8, 10, 9, 5, 6]
     #pres = [5, 5, 7, 5, 3, 8, 10, 8, 7, 6]
-    total_data = 16
+    total_data_menor_16 = 9
+    total_data_mayor_16 = 10
 
-    media_paro = np.average(paro)
-    print '\n',"media_paro: ", media_paro
-    desviacion_tipica_paro = np.std(paro)
-    print "desviacion_tipica_paro: ", desviacion_tipica_paro
+    print '\n',"la media de tasa de paro: ", np.average(paro)
 
-    media_pres =  np.average(pres)
-    print '\n',"media_pres: ", media_pres
-    desviacion_tipica_pres =  np.std(pres)
-    print "desviacion_tipica_pres: ", desviacion_tipica_pres,'\n'
+    media_paro_menor_16 = np.average(pres_paro_menor_16)
+    print '\n',"media_paro_menor_16: ", media_paro_menor_16
+    desviacion_tipica_paro_menor_16 = np.std(pres_paro_menor_16)
+    print "desviacion_tipica_paro_menor_16: ", desviacion_tipica_paro_menor_16
 
+    media_paro_mayor_16 =  np.average(pres_paro_mayor_16)
+    print '\n',"media_paro_mayot_16: ", media_paro_mayor_16
+    desviacion_tipica_paro_mayor_16 =  np.std(pres_paro_mayor_16)
+    print "desviacion_tipica_paro_mayor_16: ", desviacion_tipica_paro_mayor_16,'\n'
+
+    t = (media_paro_menor_16 - media_paro_mayor_16) / mt.sqrt((desviacion_tipica_paro_menor_16/total_data_menor_16-1) + (desviacion_tipica_paro_mayor_16/total_data_mayor_16-1))
+
+    print "t de student:", abs(t)
+
+    print "homocedasticidad: ", levene(pres_paro_mayor_16,pres_paro_menor_16)[-1]
+    value_1 = shapiro(pres_paro_menor_16)[-1]
+    print "shapiro: ", value_1
+    value_2 = shapiro(pres_paro_mayor_16)[-1]
+    print "shapiro: ", value_2
+    if value_2 > 0.05:
+        print "bingo"
+
+    '''
     mul_paro_pres = np.dot(np.array(paro), np.array(pres))
 
     covarianza_paro_pres = ((mul_paro_pres/total_data) - (media_paro * media_pres))
@@ -45,7 +63,7 @@ def covarianza():
 
     print "t de student:", t
 
-    '''
+    
     value_resta_promedio_paro = paro - media_paro
     value_resta_promedio_pres = pres - media_pres
 
